d3510c64f890595477bd17796214ae9d26068cc4
Lovro <lovro.zskrlj@gmail.com>
2025-07-28 10:22:14 +0200

    ena malenksot

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'students',
+    pathMatch: 'full'
+  },
+  {
     path: 'students',
     loadComponent: () => import('./students/students-overview').then(m => m.StudentsOverview)
   },
